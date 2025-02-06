de057c9b5425e33b24a24193cde50dceb5e6f764
Maria Goldina <mary-qa@ya.ru>
2025-02-06 10:29:21 +0500

    Add first test HelloTest

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    private String name = "Мария Голдина";
+
+    @Test
+    public void testHelloWithName(){
+        System.out.println("Hello from " + name);
+    }
+}

e9bdec11a7a4a5672137207a7b8d430325e93800
Maria Goldina <mary-qa@ya.ru>
2025-02-07 09:34:29 +0500

    Add test ApiGetTextTest

--- src/test/java/ApiGetTextTest.java
+++ src/test/java/ApiGetTextTest.java
@@ -1,12 +1,12 @@
-import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+public class ApiGetTextTest {
     @Test
-    public void testHelloWorld() {
+    public void testApiGetText() {
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
+                .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
     }

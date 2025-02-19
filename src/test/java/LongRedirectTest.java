7437d34d9816a91a27e4959b5bbe4084215451ba
Maria Goldina <mary-qa@ya.ru>
2025-02-19 15:14:01 +0500

    Update test LongRedirectTest

--- src/test/java/LongRedirectTest.java
+++ src/test/java/LongRedirectTest.java
@@ -7,6 +7,7 @@ public class LongRedirectTest {
     public void testLongRedirectLocation() {
         String url = "https://playground.learnqa.ru/api/long_redirect";
         int statusCode = 0;
+        int count = 0;
         while (statusCode!=200) {
             Response response = RestAssured
                     .given()
@@ -18,10 +19,12 @@ public class LongRedirectTest {
 
             if(response.getStatusCode()!=200) {
                 url = response.getHeader("Location");
+                count+=1;
             }
             statusCode = response.getStatusCode();
         }
 
-        System.out.println(url);
+        System.out.println("Url итогового редиректа: " + url);
+        System.out.println("Количество редиректов: " + count);
     }
 }
